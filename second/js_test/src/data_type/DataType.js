c4ac411dec971098837888f9f91ad89e8f07db0f
silenc3502 <gcccompil3r@gmail.com>
2020-11-24 03:36:27 +0900

    Add Variable

--- second/js_test/src/data_type/DataType.js
+++ second/js_test/src/data_type/DataType.js
@@ -11,6 +11,33 @@ function DataType() {
     var strTest2 = "I can 'use' this"
     var strTest3 = 'I can do "this" too'
 
+    var floatNum1 = 33.0
+    var intNum = 33
+    var floatNum2 = 37.1
+    var floatNum3 = 33.00
+
+    var expNum1 = 123e5
+    var expNum2 = 123e-5
+
+    var num1 = 3, num2 = 3, num3 = 7
+    var boolRes1 = (num1 === num2)
+    var boolRes2 = (num1 === num3)
+
+    var cars = ["BMW", "Volvo", "Audi"]
+
+    var person = {name: "Sanghoon", major: "Electronics"}
+
+    var undef
+
+    for(var i = 0; i < 3; i++) {
+        console.log("test i = " + i)
+    }
+
+    if (num1 !== 10) {
+        console.log("num !== 10")
+    } else {
+        console.log("num === 10")
+    }
 
     return (
         <div className="DataType">
@@ -30,6 +57,27 @@ function DataType() {
                 {strTest1}<br/>
                 {strTest2}<br/>
                 {strTest3}<br/>
+            </p>
+            <p>
+                {floatNum1}, {intNum}, {floatNum2}, {floatNum3}
+            </p>
+            <p>
+                {expNum1}, {expNum2}
+            </p>
+            <p>
+                {boolRes1.toString()}, {boolRes2.toString()}
+            </p>
+            <p>
+                {cars[0]}, {cars[1]}, {cars[2]}
+            </p>
+            <p>
+                {person.name}, {person.major}
+            </p>
+            <p>
+                {typeof ""}, {typeof "test"}, {typeof 0}, {typeof 3.14}, 
+                {typeof true}, {typeof undef}, {typeof undefined}, {typeof null}, <br/>
+                {typeof {name: 'Test', major: 'EE'}}, {typeof [1, 2, 3, 4]}, 
+                {typeof function test(){}}<br/>             
             </p>
         </div>
     );
